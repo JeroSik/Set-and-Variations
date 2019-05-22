2bc6e5e522b9778abaa27dbf1ff567e90372370b
Youngsik Yoon <youyoon@Youngsiks-MacBook-Pro.local>
2019-05-22 14:30:58 -0500

    Initialized Vanilla project

---
+++ VanillaSet/VanillaSet/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  VanillaSet
+//
+//  Created by Youngsik Yoon on 5/22/19.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+    }
+    
+}
