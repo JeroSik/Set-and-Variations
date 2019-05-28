e94ba978a5a180aae6f53454b7e0f9c9c02a09b0
Jerosik <yoon161@purdue.edu>
2019-05-28 15:25:56 -0500

    Initialize Graphical set

---
+++ GraphicalSet/GraphicalSet/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphicalSet
+//
+//  Created by Youngsik Yoon on 5/28/19.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
