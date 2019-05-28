0532f921f6d5006326f64a6f25bf5ecfe0d61078
Jerosik <yoon161@purdue.edu>
2019-05-28 15:30:07 -0500

    Added a deck of set cards

---
+++ GraphicalSet/GraphicalSet/Model/SetCardDeck.swift
@@ -0,0 +1,49 @@
+//
+//  SetCardDeck.swift
+//  GraphicalSet
+//
+//  Created by Youngsik Yoon on 5/28/19.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+struct SetCardDeck {
+    // Array for storing deck of cards
+    private(set) var cards = [SetCard]()
+    
+    // Draw a random card if there are cards in the deck
+    mutating func draw() -> SetCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+    
+    // Initalize deck of cards with all the unique combinations of card properties
+    init() {
+        for shape in SetCard.Shape.all {
+            for number in SetCard.Number.all {
+                for shading in SetCard.Shading.all {
+                    for color in SetCard.Color.all {
+                        cards.append(SetCard(shape: shape, number: number, shading: shading, color: color))
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Extension for generating a random number in a range
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
